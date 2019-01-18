b8f6137e03ce11f660b71a76eed4d4a5c76e44cb
Ashley <jal9626@gmail.com>
2019-01-17 23:17:45 -0500

    ashley-laneave

--- assignments/closure.js
+++ assignments/closure.js
@@ -3,7 +3,7 @@
 
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
+const counter = () => { //////
   // Return a function that when invoked increments and returns a counter variable.
 };
 // Example usage: const newCounter = counter();
